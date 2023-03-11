5dba77fdcb6fe9bb7c0ab083deefa3996baee087
Philip Carlsson <philipcarlsson@hotmail.com>
2023-03-11 12:48:20 +0100

    completing problem one

---
+++ src/components/anagram/anagramForm/index.tsx
@@ -0,0 +1,51 @@
+import { SyntheticEvent, useRef, useState } from "react";
+
+export const AnagramForm = () => {
+  const wordOneRef = useRef<HTMLInputElement>(null);
+  const wordTwoRef = useRef<HTMLInputElement>(null);
+  const [result, setResult] = useState<string | null>(null);
+
+  const checkAnagram = (e: SyntheticEvent) => {
+    e.preventDefault();
+    setResult(null);
+
+    const enteredWordOne = wordOneRef.current?.value.toLowerCase();
+    const enteredWordTwo = wordTwoRef.current?.value.toLowerCase();
+
+    if (
+      !enteredWordOne ||
+      !enteredWordTwo ||
+      enteredWordOne.length !== enteredWordTwo.length
+    ) {
+      return setResult("Please enter two words of the same length.");
+    }
+
+    if (enteredWordOne === enteredWordTwo) {
+      return setResult("Please enter two different words of the same length.");
+    }
+
+    const anagramCheckOne = enteredWordOne?.split("").sort().join("");
+    const anagramCheckTwo = enteredWordTwo?.split("").sort().join("");
+
+    if (anagramCheckOne !== anagramCheckTwo) {
+      return setResult(
+        `${enteredWordOne} and ${enteredWordTwo} are NOT anagrams!`
+      );
+    }
+
+    return setResult(`${enteredWordOne} and ${enteredWordTwo} are anagrams!`);
+  };
+
+  return (
+    <div>
+      <h2>Anagram evaluator</h2>
+      <p>Please enter two words</p>
+      <form onSubmit={checkAnagram}>
+        <input type="text" name="wordOne" ref={wordOneRef} />
+        <input type="text" name="wordOne" ref={wordTwoRef} />
+        <input type="submit" value="Check" />
+      </form>
+      {result && <p>{result}</p>}
+    </div>
+  );
+};
---
+++ src/pages/problems/1/index.tsx
@@ -0,0 +1,7 @@
+import { AnagramForm } from "@/components/anagram/anagramForm";
+
+const Problem1 = () => {
+  return <AnagramForm />;
+};
+
+export default Problem1;
