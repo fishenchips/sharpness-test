7ad5066d7eba990c9e5a375e66d0710b42c6719b
Philip Carlsson <philipcarlsson@hotmail.com>
2023-03-12 11:30:49 +0100

    installing mysql2 package

---
+++ src/pages/api/get-game-usage.ts
@@ -0,0 +1,14 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+
+type Data = {
+  name: string;
+};
+
+const handler = (req: NextApiRequest, res: NextApiResponse /* <Data> */) => {
+  if (req.method === "GET") {
+    res.status(200).json({ name: "John Doe" });
+  }
+};
+
+export default handler;
