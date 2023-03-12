b8e90060eb098f0274411d4e2355702cb07f9cb4
Philip Carlsson <philipcarlsson@hotmail.com>
2023-03-12 11:55:56 +0100

    connection to DB works

--- src/pages/api/get-game-usage.ts
+++ src/pages/api/get-game-usage.ts
@@ -1,13 +1,35 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
+import mysql from "mysql2/promise";
 
 type Data = {
   name: string;
 };
 
-const handler = (req: NextApiRequest, res: NextApiResponse /* <Data> */) => {
+const handler = async (
+  req: NextApiRequest,
+  res: NextApiResponse /* <Data> */
+) => {
   if (req.method === "GET") {
-    res.status(200).json({ name: "John Doe" });
+    const connection = await mysql.createConnection({
+      host: process.env.DB_HOST,
+      database: process.env.DB_NAME,
+      port: process.env.DB_PORT as any,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+    });
+
+    if (!connection) {
+      res.status(500).json({ message: "Error connecting to the server." });
+    }
+
+    const query = "SELECT * FROM game_usage";
+
+    const values = [];
+
+    const result = await connection.execute(query, values);
+
+    res.status(200).json({ result });
   }
 };
 
