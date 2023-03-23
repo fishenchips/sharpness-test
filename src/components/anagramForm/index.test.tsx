057c773afa01bcc96f307a94d3b24d1848b19119
Philip Carlsson <philipcarlsson@hotmail.com>
2023-03-23 11:34:02 +0100

    adding two more tests

--- src/components/anagramForm/index.test.tsx
+++ src/components/anagramForm/index.test.tsx
@@ -13,11 +13,43 @@ describe("<AnagramForm />", () => {
     const inputTwo = screen.getByTestId("wordTwo");
 
     await user.type(inputOne, "hej");
-    await user.type(inputTwo, "ehj");
+    await user.type(inputTwo, "Ehj");
     await user.click(screen.getByRole("button", { name: /check/i }));
 
     expect(
       screen.queryByText(/hej and ehj are anagrams!/i)
+    ).toBeInTheDocument();
+  });
+
+  it("If user enters the same word twice, they will recieve that feedback", async () => {
+    render(<AnagramForm />);
+
+    const inputOne = screen.getByTestId("wordOne");
+    const inputTwo = screen.getByTestId("wordTwo");
+
+    await user.type(inputOne, "same");
+    await user.type(inputTwo, "SAME");
+    await user.click(screen.getByRole("button", { name: /check/i }));
+
+    expect(
+      screen.queryByText(
+        /Please enter two different words of the same length./i
+      )
+    ).toBeInTheDocument();
+  });
+
+  it("If user enters words that aren't an anagram, they will recieve that feedback", async () => {
+    render(<AnagramForm />);
+
+    const inputOne = screen.getByTestId("wordOne");
+    const inputTwo = screen.getByTestId("wordTwo");
+
+    await user.type(inputOne, "football");
+    await user.type(inputTwo, "handball");
+    await user.click(screen.getByRole("button", { name: /check/i }));
+
+    expect(
+      screen.queryByText(/football and handball are NOT anagrams!/i)
     ).toBeInTheDocument();
   });
 });
